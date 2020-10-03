180939b9c8088bbdba09cec417fcdb6baf2339ad
abinash54 <abinash_coder269@mail.com>
2020-10-03 19:32:26 +0530

    created order system, address table, modified web.php

--- resources/views/admin/orders/orderindex.blade.php
+++ resources/views/admin/orders/orderindex.blade.php
@@ -1,6 +1,43 @@
 @extends('admin.layout.app')
 
 @section('content')
-//table for order
+<div class="container">
+    <div style="height: 150px"></div>
+    <h3>Order Table</h3>
+    <small>All the listed orders shown here</small>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Book Title</th>
+            <th scope="col">User Address</th>
+            <th scope="col">Status</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            </tr>
+        </thead>
+        
+            @if(count($orders)!=0)
+                @foreach ($orders as $order)
+            <tbody>
+                <tr>
+                    <td>{{ $order->user_id }}</td>
+                    <td>{{ $order->book_id }}</td>
+                    <td>{{ $order->address_id }}</td>
+                    <td>{{ $order->status }}</td>
+                    <td><a href="">delete request</a></td>
+                    <td><a href="">process request</a></td>
+                </tr>
+                @endforeach
+            </tbody>
+            @else
+                <p>No data available</p>
+            @endif
+            
+        
+        </table>
+
+    
+</div>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,8 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-<<<<<<< HEAD
                     
-=======
                     Debayan
->>>>>>> 043997c00eea7dbdffaf12461b4af2d60ff2ec75
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
