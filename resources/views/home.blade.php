67de1002aee20e6f9ce05a3fe97ecbfbb8437b32
ravenDC010 <csm19033@tezu.ac.in>
2020-10-03 19:06:34 +0530

    home modify

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,190 @@
         </div>
     </div>
 </div>
-@endsection
+<header class="jumbotron">
+    <div class="container">
+        <div class="row row-header">
+            <div class="col-12 offset-sm-1 col-sm-4">
+                <a class="btn btn-block btn-lg btn-warning" role="button" href="donate_form.html" id="donateBtn">DONATE BOOK</a>
+            </div>
+            <div class="col-12 offset-sm-2 col-sm-4">
+                <a class="btn btn-block btn-lg btn-warning" role="button" href="#" id="requestBtn">REQUEST BOOK</a>
+            </div>
+        </div>
+    </div>
+</header>
 
+<div class="container">
+    <div class="row row-header">
+        <div class="col-12 col-sm-6">
+            <span class="badge badge-pill bg-warning"><h3>POPULAR BOOKS</h3></span>
+        </div>
+    </div>
+    <div class="row row-content">
+        <div class="col-12 col-sm-3">
+            <a href="details.html">
+                <div class="card">
+                        <img src="pics/software_engineering_rajib_mall.png">
+                    <div class="card-header">
+                        <h3 class="card-title">Fundaments of Software Engineering</h3>
+                        Author- Rajib Mall<br>
+                    </div>
+                    <div class="card-body d-none d-sm-block">
+                        A book on software engineering fundamentals for beginners
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row row-header">
+        <div class="col-12 col-sm-6">
+            <span class="badge badge-pill bg-warning"><h3>ACADEMIC BOOKS</h3></span>
+        </div>
+    </div>
+    <div class="row row-content">
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row row-header">
+        <div class="col-12 col-sm-6">
+            <span class="badge badge-pill bg-warning"><h3>NON ACADEMIC BOOKS</h3></span>
+        </div>
+    </div>
+    <div class="row row-content">
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card view overlay">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3">
+            <div class="card">
+                    <img src="pics/software_engineering_rajib_mall.png">
+                <div class="card-header">
+                    <h3 class="card-title">Fundaments of Software Engineering</h3>
+                    Author- Rajib Mall<br>
+                </div>
+                <div class="card-body d-none d-sm-block">
+                    A book on software engineering fundamentals for beginners
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
 @extends('layouts.footer')
